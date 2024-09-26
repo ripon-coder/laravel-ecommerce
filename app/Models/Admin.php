bd38935790dd93c6fd5649da45f6d0cac2edda67
Ripon Shikder <coderripon@gmail.com>
2024-09-26 23:54:55 +0600

    seeder

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
